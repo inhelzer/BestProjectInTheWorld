c9389427a94d712b619c1d5357105447e39c8833
Liron9906 <lironplaysmc123@gmail.com>
2023-11-07 14:40:16 +0200

    Liron_finished

---
+++ GuyTheKing/Assets/Scripts/LM_player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LM_player : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private void Update()
+	{
+			
+	}
+	
+}
